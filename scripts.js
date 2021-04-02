a69559a4bd35eccfa9b05acb363b025769494333
Victor Mayer <victor.mayer@outlook.com>
2021-04-02 18:07:55 -0300

    Preço total passando pra tela de confirmação

--- scripts.js
+++ scripts.js
@@ -1,14 +1,22 @@
 let contador = 0;
 
-let category1 = false;
-let category2 = false;
-let category3 = false;
+let category_array = ['a', 'b', 'c'];
+let category_selected = [false, false, false];
 
-let selected = null;
+let selected = [null, null, null];
+
+let fecharPedido = false;
+
+let price_array = [0, 0, 0];
+let item_title = "";
 
 function confirmarPedido() {
-    console.log("botao está sendo clicado");
-    console.log("botao está sendo clicado");
+    let total = 0;
+    for (let i = 0; i < price_array.length; i++) {
+        total += price_array[i];
+    }
+    console.log(total.toFixed(2));
+    document.querySelector(".cart-item.total .total-price").innerHTML = total.toFixed(2);
     const elemento = document.querySelector(".fade");
     elemento.classList.remove("hidden");
 }
@@ -19,90 +27,50 @@ function cancelar() {
     elemento.classList.add("hidden");
 }
 
-function selectCard(category, card) {
-    console.log("entra na função de seleção");
-    if (category === 1) {
-        console.log("cartão está sendo selecionado");
-        const card_toString = ".a" + card;
-        const elemento = document.querySelector(card_toString);
-        elemento.classList.add("selected");
-        selected = elemento;
-        contador++;
-        category1 = true;
-    } else if (category === 2) {
-        console.log("cartão está sendo selecionado");
-        const card_toString = ".b" + card;
-        const elemento = document.querySelector(card_toString);
-        elemento.classList.add("selected");
-        selected = elemento;
-        contador++;
-        category2 = true;
-    } else if (category === 3) {
-        console.log("cartão está sendo selecionado");
-        const card_toString = ".c" + card;
-        const elemento = document.querySelector(card_toString);
-        elemento.classList.add("selected");
-        selected = elemento;
-        contador++;
-        category3 = true;
-    } else {
-        prompt("Um erro inesperado ocorreu. Por favor, entre em contato com o desenvolvedor")
-    }
-}
-
-function deselectCard(category, card) {
-    console.log("entra na função de deseleção");
-    if (category === 1) {
-        console.log("cartão está sendo deselecionado");
-        const cardSelected_toString = ".a" + card + ".selected";
-        // const elemento = document.querySelector(cardSelected_toString);
-        selected.classList.remove("selected");
-        contador--;
-        category1 = false;
-    } else if (category === 2) {
-        console.log("cartão está sendo deselecionado");
-        const cardSelected_toString = ".b" + card + ".selected";
-        // const elemento = document.querySelector(cardSelected_toString);
-        selected.classList.remove("selected");
-        contador--;
-        category2 = false;
-    } else if (category === 3) {
-        console.log("cartão está sendo deselecionado");
-        const cardSelected_toString = ".c" + card + ".selected";
-        // const elemento = document.querySelector(cardSelected_toString);
-        selected.classList.remove("selected");
-        contador--;
-        category3 = false;
-    }
-}
-
 // function
 
 function clicked(category, card) {
-    switch (category) {
-        case 1:
-            if (category1 === true) {
-                deselectCard(1, card)
-            } else {
-                selectCard(1, card)
+    const card_toString = "." + category + card;
+    const price = parseFloat(document.querySelector(card_toString + " .price .hidden").innerHTML);
+    for (let index = 0; index < category_array.length; index++) {
+        //seleciona a categoria
+        if (category_array[index] === category) {
+            const elemento = document.querySelector(card_toString);
+            // clicou em um card
+            if (category_selected[index] === false) {
+                elemento.classList.add("selected");
+                price_array[index] = price;
+                selected[index] = elemento;
+                contador++;
+                category_selected[index] = true;
             }
-            break;
-        case 2:
-            if (category2 === true) {
-                deselectCard(2, card)
-            } else {
-                selectCard(2, card)
+            // clicou em um card diferente
+            else if (elemento !== selected[index]) {
+                price_array[index] = price;
+                selected[index].classList.remove("selected");
+                selected[index] = elemento;
+                elemento.classList.add("selected");
             }
-            break;
-        case 3:
-            if (category3 === true) {
-                deselectCard(3, card)
-            } else {
-                selectCard(3, card)
+            // clicou no card selecionado
+            else {
+                price_array[index] = 0;
+                selected[index].classList.remove("selected");
+                selected[index] = null;
+                category_selected[index] = false;
+                contador--;
             }
-            break;
-        default:
-            break;
+        }
+    }
+    habilitarPedido(contador);
+}
 
+function habilitarPedido(contador) {
+    const elemento = document.querySelector(".habilitado");
+    if (contador === 3) {
+        fecharPedido = true;
+        elemento.classList.remove("hidden");
+    } else if (fecharPedido) {
+        fecharPedido = false;
+        elemento.classList.add("hidden");
     }
 }
