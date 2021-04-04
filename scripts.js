dcf2b6256eb5a2ee8b9c1c6b7557cd3c7aa03be9
Victor Mayer <victor.mayer@outlook.com>
2021-04-04 11:51:15 -0300

    falta enviar para o whats

--- scripts.js
+++ scripts.js
@@ -7,19 +7,11 @@ let selected = [null, null, null];
 
 let fecharPedido = false;
 
-let price_array = [0, 0, 0];
-let item_title = "";
-
-function confirmarPedido() {
-    let total = 0;
-    for (let i = 0; i < price_array.length; i++) {
-        total += price_array[i];
-    }
-    console.log(total.toFixed(2));
-    document.querySelector(".cart-item.total .total-price").innerHTML = total.toFixed(2);
-    const elemento = document.querySelector(".fade");
-    elemento.classList.remove("hidden");
-}
+let price_array_number = [0, 0, 0];
+let price_array_string = ["", "", ""];
+let itens_array = ["", "", ""];
+const cart_itens_title = [".food-title", ".drink-title", ".desert-title"];
+const cart_itens_price = [".food-price", ".drink-price", ".desert-price"];
 
 function cancelar() {
     console.log("botao está sendo clicado");
@@ -27,33 +19,59 @@ function cancelar() {
     elemento.classList.add("hidden");
 }
 
-// function
+function confirmarPedido() {
+    let total = 0;
+    let total_toString = "";
+    for (let i = 0; i < price_array_number.length; i++) {
+        total += price_array_number[i];
+    }
+
+    for (let i = 0; i < price_array_string.length; i++) {
+        document.querySelector(cart_itens_price[i]).innerHTML = price_array_string[i].substr(3);
+    }
+
+    for (let i = 0; i < itens_array.length; i++) {
+        document.querySelector(cart_itens_title[i]).innerHTML = itens_array[i];
+    }
+    total_toString = "R$ " + total.toFixed(2).toString().replace(".", ",");
+    document.querySelector(".cart-item.total .total-price").innerHTML = total_toString;
+    const elemento = document.querySelector(".fade");
+    elemento.classList.remove("hidden");
+}
 
 function clicked(category, card) {
     const card_toString = "." + category + card;
-    const price = parseFloat(document.querySelector(card_toString + " .price .hidden").innerHTML);
+    let price_number = parseFloat(document.querySelector(card_toString + " .price .hidden").innerHTML);
+    let price_string = document.querySelector(card_toString + " .price p:first-child").innerHTML;
+    let item_title = document.querySelector(card_toString + " .item-title").innerHTML;
+    //seleciona a categoria
     for (let index = 0; index < category_array.length; index++) {
-        //seleciona a categoria
         if (category_array[index] === category) {
             const elemento = document.querySelector(card_toString);
             // clicou em um card
             if (category_selected[index] === false) {
                 elemento.classList.add("selected");
-                price_array[index] = price;
+                price_array_number[index] = price_number;
+                price_array_string[index] = price_string;
+                itens_array[index] = item_title;
                 selected[index] = elemento;
                 contador++;
                 category_selected[index] = true;
             }
             // clicou em um card diferente
             else if (elemento !== selected[index]) {
-                price_array[index] = price;
+                price_array_number[index] = price_number;
+                price_array_string[index] = price_string;
+                itens_array[index] = item_title;
                 selected[index].classList.remove("selected");
                 selected[index] = elemento;
                 elemento.classList.add("selected");
             }
             // clicou no card selecionado
             else {
-                price_array[index] = 0;
+                price_array_number[index] = 0;
+                price_array_string[index] = "";
+                itens_array[index] = "";
                 selected[index].classList.remove("selected");
                 selected[index] = null;
                 category_selected[index] = false;
